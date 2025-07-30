76ccb969d1fc8cf7f913e123f68ee13fc863e4d5
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-07-30 13:18:22 +0600

    compare

--- comparison.js
+++ comparison.js
@@ -1,9 +1,35 @@
 // COMPARISON
 //  1. bigger: >
-// 2.less : < 
+// 2.less : <
 //  3. equal : ==
-//  4. greater than or equal: >= 
+//  4. greater than or equal: >=
 //  5. less than or equal: <=
 //  not equal : !=
 //  and: &&
 //  or : ||
+
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(5 == 10.1);
+console.log(10 >= 10);
+console.log(15 >= 10);
+console.log(10 <= 10);
+console.log(5 <= 10);
+console.log(100 <= 10);
+
+var peyaraPrice = 100;
+var applePrice = 250;
+
+console.log(peyaraPrice > applePrice);
+console.log(applePrice < peyaraPrice);
+
+console.log(10 == "10");
+// // output true
+console.log(10 === "10");
+// // output false
+
+console.log(10 != "10");
+// output false
+console.log(10 !== "10");
+// output true
