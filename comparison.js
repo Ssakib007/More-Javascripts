bb698fd4b66d9487c9decadc6d94c9f787106e2a
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-07-30 13:03:38 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,9 @@
+// COMPARISON
+//  1. bigger: >
+// 2.less : < 
+//  3. equal : ==
+//  4. greater than or equal: >= 
+//  5. less than or equal: <=
+//  not equal : !=
+//  and: &&
+//  or : ||
---
+++ varable.js
@@ -0,0 +1,28 @@
+// numeric
+
+var sunglassPrice = 80;
+
+// string
+
+var subject = "EEE";
+
+// boolean
+
+var passed = ture;
+
+// naming convention
+// 1. single word
+// 2. no quote
+//  no gap or dash
+//  can not start with number
+//  can not be a keyword
+//  prefer camelCase
+
+var lastExamResult = 89;
+
+// operation
+// 1. +, -, *, /, %
+// 2. +=, -=, *=, /=
+// 3. isNaN
+// 4. null
+// 5. number
